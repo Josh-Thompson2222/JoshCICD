26a66e51be2ec931c4211423bbd48f83fcdf6b02
Josh <G00422756@atu.ie>
2024-10-09 15:44:20 +0100

    Added Product, Productcontroller and GlobalErrorHandler

--- src/main/java/com/example/cicd4/Product.java
+++ src/main/java/com/example/cicd4/Product.java
@@ -1,4 +1,34 @@
 package com.example.cicd4;
 
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @NotBlank(message="You must enter a name")
+    private String name;
+
+    @Min(value=1, message="Must be greater or equal to 1")
+    private int id;
+
+    @NotBlank(message="You must enter an age")
+    private String age;
+
+    @NotBlank(message="You must enter an age")
+    private String title;
+
+    @NotBlank(message="You must enter a position")
+    private String position;
+
+    @Email(message = "Not valid email")
+    private String email;
 }
